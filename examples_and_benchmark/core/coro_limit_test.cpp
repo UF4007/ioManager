94c066919cdf866e70ede9d7cedca90deb1c807b
UF4007 <142472038+UF4007@users.noreply.github.com>
2025-11-13 09:54:01 +0800

    Update coro_limit_test.cpp

--- examples_and_benchmark/core/coro_limit_test.cpp
+++ examples_and_benchmark/core/coro_limit_test.cpp
@@ -15,10 +15,18 @@ io::fsm_func<void> limit_test(int num)
                     fsm.make_clock(cl1, std::chrono::seconds(1));
                     fsm.make_clock(cl2, std::chrono::seconds(2));
                     io::promise prom = fsm.make_future(fut);
-                    do {
-                        auto tag = co_await io::future::race(cl1, cl2, fut);
-                        if (tag == cl1) {}
-                    } while (0);;
+                    IO_SELECT_BEGIN(io::future::race(cl1, cl2, fut))
+                        // default scenario:
+                        //  - all the future had successed when in io::future::all
+                        //  - all the future had error when in io::future::any
+                        //  - use io::future::allSettle
+                    IO_SELECT(cl1)
+                        // clock 1 triggered
+                    IO_SELECT(cl2)
+                        // clock 2 triggered
+                    IO_SELECT(fut)
+                        // fut triggered
+                    IO_SELECT_END
                 } }())
             .detach();
     }
@@ -27,7 +35,8 @@ io::fsm_func<void> limit_test(int num)
 int main()
 {
     io::manager mngr;
-    mngr.async_spawn(limit_test(10000000));
+    mngr.async_spawn(limit_test(1000000));
+    // mngr.async_spawn(limit_test(10000000)); //Up to 10M coroutines are supported and it will cost about 7GB RAM.
 
     while (1)
     {
